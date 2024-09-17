f7d1799e525d54635676a1e871724f8f0e5eba49
lprosi <133262517+lprosi@users.noreply.github.com>
2024-09-17 18:16:10 +0300

    Добавьте файлы проекта.

---
+++ snakeOriginal/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace snakeOriginal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.coin = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // coin
+            // 
+            this.coin.BackColor = System.Drawing.Color.Yellow;
+            this.coin.Location = new System.Drawing.Point(500, 200);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(50, 50);
+            this.coin.TabIndex = 1;
+            this.coin.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(754, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(46, 18);
+            this.label.TabIndex = 2;
+            this.label.Text = "Счет";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.Location = new System.Drawing.Point(771, 27);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(17, 18);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.coin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox coin;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ snakeOriginal/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snakeOriginal
+{
+    public partial class Form1 : Form
+    {
+        private int fieldSize = 50;
+        private int rTop, rLeft;
+        private PictureBox[] snake1 = new PictureBox [200];
+        private int score = 0;
+        private Keys _lastMove = Keys.None;
+      
+        private void Create_Boxes()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    PictureBox field = new PictureBox();
+                    field.Size = new Size(fieldSize, fieldSize);
+                    field.BorderStyle = BorderStyle.FixedSingle;
+                    field.Location = new Point(j * fieldSize, fieldSize * i); 
+                    this.Controls.Add(field);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Create_Boxes();
+            this.KeyDown += new KeyEventHandler(Move);
+            snake1[0] = new PictureBox();
+            snake1[0].Size = new Size(fieldSize, fieldSize);
+            snake1[0].Location = new Point(300, 300);
+            snake1[0].BackColor = Color.DodgerBlue;
+            snake1[0].BorderStyle= BorderStyle.FixedSingle;
+            this.Controls.Add(snake1[0]);
+            snake1[0].BringToFront();
+            
+        }
+        
+        private void Move(object sender, KeyEventArgs e)
+        {
+            for (int i = score; i >= 0; i--)
+            {
+                if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && timer1.Enabled == true)
+                {
+                    if (i == 0)
+                    {
+                        snake1[i].Location = new Point(snake1[i].Location.X + fieldSize, snake1[i].Location.Y);
+                    }
+                    else
+                    {
+                        PictureBox[] temp = new PictureBox[1];
+                        temp[0] = snake1[i - 1];
+                        snake1[i].Location = new Point(temp[0].Location.X, temp[0].Location.Y);
+                    }
+                    
+
+                }
+                if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && timer1.Enabled == true)
+                {
+                    if (i == 0)
+                    {
+                        snake1[i].Location = new Point(snake1[i].Location.X - fieldSize, snake1[i].Location.Y);
+                    }
+                    else
+                    {
+                        PictureBox[] temp = new PictureBox[1];
+                        temp[0] = snake1[i - 1];
+                        snake1[i].Location = new Point(temp[0].Location.X, temp[0].Location.Y);
+                    }
+                    
+
+                }
+                if ((e.KeyCode == Keys.Up || e.KeyCode == Keys.W) && timer1.Enabled == true)
+                {
+                    if (i == 0)
+                    {
+                        snake1[i].Location = new Point(snake1[i].Location.X, snake1[i].Location.Y - fieldSize);
+                    }
+                    else
+                    {
+                        PictureBox[] temp = new PictureBox[1];
+                        temp[0] = snake1[i - 1];
+                        snake1[i].Location = new Point(temp[0].Location.X, temp[0].Location.Y);
+                    }
+                    
+
+                }
+                if ((e.KeyCode == Keys.Down || e.KeyCode == Keys.S) && timer1.Enabled == true)
+                {
+                    if (i == 0)
+                    {
+                        snake1[i].Location = new Point(snake1[i].Location.X, snake1[i].Location.Y + fieldSize);
+                    }
+                    else
+                    {
+                        PictureBox[] temp = new PictureBox[1];
+                        temp[0] = snake1[i - 1];
+                        snake1[i].Location = new Point(temp[0].Location.X, temp[0].Location.Y );
+                    }
+
+                }
+                
+            }
+            
+        }
+        private void AddSnake()
+        {
+            snake1[score] = new PictureBox();
+            snake1[score].BackColor= Color.Blue;
+            snake1[score].Size = new Size(fieldSize, fieldSize);
+            PictureBox[] temp = new PictureBox[1];
+            temp[0] = snake1[score - 1];
+            snake1[score].Location = new Point(temp[0].Location.X, temp[0].Location.Y);
+            snake1[score].BorderStyle = BorderStyle.FixedSingle;
+            this.Controls.Add(snake1[score]);
+            snake1[score].BringToFront();
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void CoinEat()
+        {
+            Random random = new Random();
+            rLeft = random.Next(0, 750);
+            int diff1 = rLeft % fieldSize;
+            rLeft = rLeft - diff1;
+            rTop = random.Next(0, 600);
+            int diff2 = rTop % fieldSize;
+            rTop = rTop - diff2;
+            coin.Location = new Point(rLeft, rTop);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (snake1[0].Top < 0 || snake1[0].Top > 550)
+            {
+                timer1.Enabled = false;
+            }
+            if (snake1[0].Left < 0 || snake1[0].Left > 700)
+            {
+                timer1.Enabled = false;
+            }
+
+            if (snake1[0].Bounds.IntersectsWith(coin.Bounds))
+            {
+
+                CoinEat();
+                for (int i = score; i >= 0; i--)
+                {
+
+
+                    
+                    if (snake1[i].Bounds.IntersectsWith(coin.Bounds))
+                    {
+                        CoinEat();
+                        i = score;
+                    }
+                    
+                }
+                score++;
+                AddSnake();
+                scoreLabel.Text = score.ToString();
+            }
+            if (score == 180)
+            {
+                timer1.Enabled = false;
+            }
+
+            for (int i = score; i >= 2 ; i--)
+            {
+                if (snake1[0].Bounds.IntersectsWith(snake1[i].Bounds))
+                {
+                    timer1.Enabled = false;
+                }
+            }
+
+
+        }
+    }
+}
